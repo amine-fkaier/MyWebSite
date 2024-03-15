09a2cf59d4ba76b672ba463f6552d475b7f09415
amine-fkaier <aminefkaier@gmail.com>
2024-03-15 08:01:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("hellooooooooooo")
+  console.log("helloo")
   return (
     <div className="App">
       <header className="App-header">
