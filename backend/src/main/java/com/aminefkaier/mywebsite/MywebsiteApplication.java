bf997c2ec9b02c67787f9848a4317745598ecdab
Amine <aminefkaier@gmail.com>
2024-03-19 00:19:55 +0100

    first commit

--- backend/src/main/java/com/aminefkaier/mywebsite/MywebsiteApplication.java
+++ backend/src/main/java/com/aminefkaier/mywebsite/MywebsiteApplication.java
@@ -14,6 +14,7 @@ public class MywebsiteApplication {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 
 	}
 
