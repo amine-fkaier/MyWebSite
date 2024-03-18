06b0f816642b657d3ec1210153de75af0fbb5086
Amine <aminefkaier@gmail.com>
2024-03-18 14:52:01 +0100

    Initial commit

---
+++ backend/src/main/java/com/aminefkaier/mywebsite/MywebsiteApplication.java
@@ -0,0 +1,20 @@
+package com.aminefkaier.mywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebsiteApplication.class, args);
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+
+	}
+
+
+
+}
